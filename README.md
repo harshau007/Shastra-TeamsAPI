f3d6c9e929810da49d6a52800a965866192bdf42
harshau007 <amanupadhyay2004@gmail.com>
2023-05-05 10:45:42 +0530

    Added commands in readme

--- README.md
+++ README.md
@@ -18,6 +18,36 @@ To deploy this project run
 ```
 
 
+## Commands
+
+**GET Methods**
+| Command | Description |
+| --- | --- |
+| `/coreMembers` | List all core team members |
+| `/coreMember/:id` | List specific core team members |
+| `/subcoreMembers` | List all subcore team members |
+| `/subcoreMember/:id` | List specific subcore team members |
+| `/events` | List all current events |
+| `/event/:id` | List specific current event |
+
+**POST Methods**
+| Command | Description |
+| --- | --- |
+| `/addCoreteamMember` | Add core team member |
+| `/addSubcoreteamMember` | Add subcore team member |
+
+**PUT Methods**
+| Command | Description |
+| --- | --- |
+| `/updateCoreMember/:id` | Update specific core team member |
+| `/updateSubCoreMember/:id` | Update specific subcore team member |
+
+**DELETE Methods**
+| Command | Description |
+| --- | --- |
+| `/deleteCoreMember/:id` | Delete specific core team member |
+| `/deleteSubcoreMember/:id` | Delete specific subcore team member |
+
 ## Tech Stack
 
 **Language:** JavaScript
--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var port = process.env.PORT || 3000;
 
 app.listen(port, '0.0.0.0');
 app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/teams', (req, res) => {
     res.send("Welcome to Shastra Coding Club's Team Section")
@@ -18,15 +18,15 @@ app.get('/teams', (req, res) => {
 
 // Connected to DataBase
 mongoose.
-connect(process.env.DataBaseURI)
-.then(()=>{
-    console.log("Connected to MongoDB")
-    app.listen(3000, ()=>{
-        console.log("App running on http://localhost:3000")
+    connect(process.env.DataBaseURI)
+    .then(() => {
+        console.log("Connected to MongoDB")
+        app.listen(3000, () => {
+            console.log("App running on http://localhost:3000")
+        })
+    }).catch((error) => {
+        console.log(error)
     })
-}).catch((error) => {
-    console.log(error)
-})
 
 
 app.get('/', (req, res) => {
@@ -36,164 +36,164 @@ app.get('/', (req, res) => {
 /*******************Members Section*********************/
 
 // Add core members
-app.post('/addCoreteamMember', async(req, res) => {
+app.post('/addCoreteamMember', async (req, res) => {
     try {
         const member = await coreteamsModel.create(req.body)
         res.status(200).json(member)
     } catch (error) {
         console.log(error.message);
-        res.status(500).json({message: error.message})
+        res.status(500).json({ message: error.message })
     }
 })
 
 // Add subcore members
-app.post('/addSubcoreteamMember', async(req, res) => {
+app.post('/addSubcoreteamMember', async (req, res) => {
     try {
         const member = await subcoreteamsModel.create(req.body)
         res.status(200).json(member)
     } catch (error) {
         console.log(error.message);
-        res.status(500).json({message: error.message})
+        res.status(500).json({ message: error.message })
     }
 })
 
 
 // Get Core Members
-app.get('/coreMembers', async(req, res) => {
+app.get('/coreMembers', async (req, res) => {
     try {
         const coreMembers = await coreteamsModel.find({});
         res.status(200).json(coreMembers);
     } catch (error) {
-        res.status(500).json({message: error.message})
+        res.status(500).json({ message: error.message })
     }
 })
 
-app.get('/coreMember/:id', async(req, res) => {
+app.get('/coreMember/:id', async (req, res) => {
     try {
-        const {id} = req.params;
+        const { id } = req.params;
         const coreMember = await coreteamsModel.findById(id);
         res.status(200).json(coreMember);
     } catch (error) {
-        res.status(500).json({message: error.message})
+        res.status(500).json({ message: error.message })
     }
 })
 
 
 // Get Sub core members
-app.get('/subcoreMembers', async(req, res) => {
+app.get('/subcoreMembers', async (req, res) => {
     try {
         const subcoreMembers = await subcoreteamsModel.find({});
         res.status(200).json(subcoreMembers);
     } catch (error) {
-        res.status(500).json({message: error.message})
+        res.status(500).json({ message: error.message })
     }
 })
 
-app.get('/subcoreMember/:id', async(req, res) => {
+app.get('/subcoreMember/:id', async (req, res) => {
     try {
-        const {id} = req.params;
+        const { id } = req.params;
         const subcoreMember = await subcoreteamsModel.findById(id);
         res.status(200).json(subcoreMember);
     } catch (error) {
-        res.status(500).json({message: error.message})
+        res.status(500).json({ message: error.message })
     }
 })
 
 
 // Update core members
-app.put('/updateCoreMember/:id', async(req, res) => {
+app.put('/updateCoreMember/:id', async (req, res) => {
     try {
-        const {id} = req.params;
+        const { id } = req.params;
         const member = await coreteamsModel.findByIdAndUpdate(id, req.body);
         // we cannot find any product in database
-        if(!member){
-            return res.status(404).json({message: `cannot find any Member with ID ${id}`})
+        if (!member) {
+            return res.status(404).json({ message: `cannot find any Member with ID ${id}` })
         }
         const updatedMember = await coreteamsModel.findById(id);
         res.status(200).json(updatedMember);
-        
+
     } catch (error) {
-        res.status(500).json({message: error.message})
+        res.status(500).json({ message: error.message })
     }
 })
 
 // Update subcore members
-app.put('/updateSubCoreMember/:id', async(req, res) => {
+app.put('/updateSubCoreMember/:id', async (req, res) => {
     try {
-        const {id} = req.params;
+        const { id } = req.params;
         const member = await subcoreteamsModel.findByIdAndUpdate(id, req.body);
         // we cannot find any product in database
-        if(!member){
-            return res.status(404).json({message: `cannot find any Member with ID ${id}`})
+        if (!member) {
+            return res.status(404).json({ message: `cannot find any Member with ID ${id}` })
         }
         const updatedMember = await subcoreteamsModel.findById(id);
         res.status(200).json(updatedMember);
-        
+
     } catch (error) {
-        res.status(500).json({message: error.message})
+        res.status(500).json({ message: error.message })
     }
 })
 
 // Delete core Member
-app.delete('/deleteCoreMember/:id', async(req, res) =>{
+app.delete('/deleteCoreMember/:id', async (req, res) => {
     try {
-        const {id} = req.params;
+        const { id } = req.params;
         const member = await coreteamsModel.findByIdAndDelete(id);
-        if(!member){
-            return res.status(404).json({message: `cannot find any product with ID ${id}`})
+        if (!member) {
+            return res.status(404).json({ message: `cannot find any product with ID ${id}` })
         }
         res.status(200).json(member);
-        
+
     } catch (error) {
-        res.status(500).json({message: error.message})
+        res.status(500).json({ message: error.message })
     }
 })
 
 // Delete Subcore member
-app.delete('/deleteSubcoreMember/:id', async(req, res) =>{
+app.delete('/deleteSubcoreMember/:id', async (req, res) => {
     try {
-        const {id} = req.params;
+        const { id } = req.params;
         const member = await subcoreteamsModel.findByIdAndDelete(id);
-        if(!member){
-            return res.status(404).json({message: `cannot find any product with ID ${id}`})
+        if (!member) {
+            return res.status(404).json({ message: `cannot find any product with ID ${id}` })
         }
         res.status(200).json(member);
-        
+
     } catch (error) {
-        res.status(500).json({message: error.message})
+        res.status(500).json({ message: error.message })
     }
 })
 
 /*******************Event Section*********************/
 
 // Add event
-app.post('/addEvent', async(req, res) => {
+app.post('/addEvent', async (req, res) => {
     try {
         const event = await currentEvent.create(req.body)
         res.status(200).json(event)
     } catch (error) {
         console.log(error.message);
-        res.status(500).json({message: error.message})
+        res.status(500).json({ message: error.message })
     }
 })
 
 
 // Get Event
-app.get('/events', async(req, res) => {
+app.get('/events', async (req, res) => {
     try {
         const events = await currentEvent.find({});
         res.status(200).json(events);
     } catch (error) {
-        res.status(500).json({message: error.message})
+        res.status(500).json({ message: error.message })
     }
 })
 
-app.get('/event/:id', async(req, res) => {
+app.get('/event/:id', async (req, res) => {
     try {
-        const {id} = req.params;
+        const { id } = req.params;
         const event = await currentEvent.findById(id);
         res.status(200).json(event);
     } catch (error) {
-        res.status(500).json({message: error.message})
+        res.status(500).json({ message: error.message })
     }
 })
